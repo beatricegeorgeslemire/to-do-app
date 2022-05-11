0c09b97a3c3ccd6e11b9dda3c1eed538365f5bb5
Beatrice Georges-Lemire <ladybug2503@gmail.com>
2022-05-11 16:48:38 -0400

    completed task addition functionality

--- app.js
+++ app.js
@@ -1,15 +1,73 @@
+//Query the DOM for the form element
+
+const formElement = document.querySelector('form');
+console.log(formElement);
 
 //Add a submit event listener on the form
-//
-//prevent the page from refreshing whent he form is submitted
+formElement.addEventListener('submit', function (event) {
+    //prevent the page from refreshing whent he form is submitted
 
-//only if the user has entered an actual task (aka is not empty)
 
-//grab the user's to do item form the form input
-//display the TO DO on the page within an li element
-    //include a checkbox icon within the li
-//add li element to the ul
+    //only if the user has entered an actual task (aka is not empty)
+    event.preventDefault();
+    //log out he event object which is generated when the form is submitted
+    console.log(event);
+
+    alert(`You have submitted this form!`);
+    //query the DOM for the input element and check whether its empty
+    const inputElement = document.getElementById('toDoItem');
+    console.log(inputElement);
+
+    //only if the user has submitted an actual task
+    if (inputElement.value !== '') {
+
+        console.log('congrats on entering a task');
+
+        //grab the user's to do item form the form input
+        console.log(inputElement.value);
+
+        //create an li
+        const liElement = document.createElement('li');
+        console.log(liElement);
+
+        //display the TO DO on the page within an li element
+
+        //include a checkbox icon within the li
+        liElement.innerHTML = `<i class="fa-regular fa-square"></i>`;
+        //create an element that represents the text we have to add (our TO DO)
+        const toDoContent = document.createTextNode(inputElement.value);
+        console.log(toDoContent);
+
+        //and then append that text element to the li
+        liElement.appendChild(toDoContent);
+
+        //add li element to the ul
+        document.querySelector('ul').appendChild(liElement);
+
+        //clear input!
+        inputElement.value = '';
+
+    } else {
+        alert("Stop bein lazy and submit something");
+    }
+
+
+
+});
+
+
+
 //clicking on a task allows you to toggle between checked vs unchecked (aka done vs not done)
+
+//NOTE: this will not work because you can only add event listeners to elements which exist at the time of the code execution.
+
+// const listElements = document.querySelector('li');
+// listElements.addEventListener('click', function () {
+//     console.log('to do has been checked!');
+// });
+
+//in order to attach a click event listener to the li's which do not exist on th epage yet, we can use :
+//EVENT PROPAGATION to DELEGATE the click event to the ul!
 
 //BONUS LEVEL
 //add a reset button which clears out all of the TO DOS
